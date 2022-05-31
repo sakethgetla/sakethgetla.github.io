f0ebbd57198d47d2b52c9c5acb803231b0733965
sakethgetla <saketh.getla@protonmail.com>
2022-05-31 20:58:25 +1000

    added links and more projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { AppBar, Box, Button, Card, CardContent, CardMedia, CardActionArea, Toolbar, Typography, IconButton } from '@mui/material';
+import { AppBar, Box, Button, Card, CardContent, CardMedia, CardActionArea, Grid, Toolbar, Typography, IconButton } from '@mui/material';
+// import { useNavigate } from "react-router-dom";
 // import {MenuIcon} from '@mui/icons-material/Menu'
 
-function App() {
-  const pages = ['Products', 'Pricing', 'Blog'];
 
+function App() {
+  // const pages = ['Products', 'Pricing', 'Blog'];
+  const pages = [
+    { title: "Path Finder with Solidjs", alt: "image of algorithm", body: " Use Astar, Dijkstra and Heurisic algorithms to find a path through the map.", picture: "/static/images/cards/pathFind.jpg", link: "https://sakethgetla.github.io/PathFinder_Solidjs/" },
+    { title: "Path Finder with React", alt: "image of algorithm", body: " Use Astar, Dijkstra and Heurisic algorithms to find a path through the map.", picture: "/static/images/cards/pathFind.jpg", link: "https://sakethgetla.github.io/game_site/" },
+    { title: "Q learning [not finished]", alt: "image of algorithm", body: "Q learing algorithm to eat the correct food", picture: "/static/images/cards/scene.jpg", link: "https://sakethgetla.github.io/game_site/" }
+  ]
+
+  // function redir() {
+  // let a = useNavigate();
+  // const redir = () => {
+  //   a("https://sakethgetla.github.io/game_site/pathFinder");
+  // }
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
@@ -27,32 +39,46 @@ function App() {
           </Toolbar>
         </AppBar>
       </Box>
-      <div className="App">
-        <div>
-          app
-        </div>
-        <a href="https://sakethgetla.github.io/game_site/">
-          game site
-        </a>
-      </div>
-    <Card sx={{ width: 1/2, maxWidth:350 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="100%"
-            image="/static/images/cards/pathFind.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Path Finding Algorithms
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Use Astar, Dijkstra and Heurisic algorithms to find a path through the map.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      {/* <div className="App"> */}
+      {/*   <div> */}
+      {/*     app */}
+      {/*   </div> */}
+      {/*   <a href="https://sakethgetla.github.io/game_site/"> */}
+      {/*     game site */}
+      {/*   </a> */}
+      {/* </div> */}
+      {/* <Grid container spacing={5} padding={5}> */}
+      <Grid container spacing={5} paddingTop={5}>
+        {
+          pages.map(page => (
+            <Grid item xs={4} lg={2} >
+              <Card sx={{ padding: 1, margin: 1, width: 1, height:1 }} >
+              {/* <Card > */}
+                <CardActionArea
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = page.link;
+                  }} >
+                  <CardMedia
+                    component="img"
+                    height="100%"
+                    image={page.picture}
+                    alt={page.alt}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {page.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {page.body}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </>
   );
 }
