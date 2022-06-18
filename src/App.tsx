aab300b49fc5af345646c1ed27b59b6755db8a44
sakethgetla <saketh.getla@protonmail.com>
2022-06-18 20:55:43 +1000

    added new image and aligned images better.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { AppBar, Box, Button, Card, CardContent, CardMedia, CardActionArea, Grid, Toolbar, Typography, IconButton } from '@mui/material';
 // import { useNavigate } from "react-router-dom";
 // import {MenuIcon} from '@mui/icons-material/Menu'
@@ -7,9 +8,10 @@ import { AppBar, Box, Button, Card, CardContent, CardMedia, CardActionArea, Grid
 function App() {
   // const pages = ['Products', 'Pricing', 'Blog'];
   const pages = [
-    { title: "Path Finder with Solidjs", alt: "image of algorithm", body: " Use Astar, Dijkstra and Heurisic algorithms to find a path through the map.", picture: "/static/images/cards/pathFind.jpg", link: "https://sakethgetla.github.io/PathFinder_Solidjs/" },
-    { title: "Path Finder with React", alt: "image of algorithm", body: " Use Astar, Dijkstra and Heurisic algorithms to find a path through the map.", picture: "/static/images/cards/pathFind.jpg", link: "https://sakethgetla.github.io/PathFinder_React/" },
-    { title: "Q learning [not finished]", alt: "image of algorithm", body: "Q learing algorithm to eat the correct food", picture: "/static/images/cards/scene.jpg", link: "https://sakethgetla.github.io/game_site/" }
+    {id: 1, title: "Path Finder with solid.js", alt: "image of algorithm", body: " Use Astar, Dijkstra and Heurisic algorithms to find a path through the map.", picture: "/static/images/cards/PathFinderSolidjs.jpg", link: "https://sakethgetla.github.io/PathFinder_Solidjs/" },
+    {id: 2,  title: "Path Finder with react.js", alt: "image of algorithm", body: " Use Astar, Dijkstra and Heurisic algorithms to find a path through the map.", picture: "/static/images/cards/pathFind.jpg", link: "https://sakethgetla.github.io/PathFinder_React/" },
+    {id: 3,  title: "Q learning with solid.js & planck.js", alt: "image of algorithm", body: "Q learing algorithm to eat the food", picture: "/static/images/cards/AI_agent.jpg", link: "https://sakethgetla.github.io/QLearningGames/" },
+    {id: 4,  title: "Q learning with react & matter.js [not finished]", alt: "image of algorithm", body: "Q learing algorithm to eat the correct food", picture: "/static/images/cards/scene.jpg", link: "https://sakethgetla.github.io/game_site/" },
   ]
 
   // function redir() {
@@ -51,7 +53,7 @@ function App() {
       <Grid container spacing={5} paddingTop={5}>
         {
           pages.map(page => (
-            <Grid item xs={4} lg={2} >
+            <Grid key={page.id} item xs={4} lg={2} >
               <Card sx={{ padding: 1, margin: 1, width: 1, height:1 }} >
               {/* <Card > */}
                 <CardActionArea
@@ -61,7 +63,9 @@ function App() {
                   }} >
                   <CardMedia
                     component="img"
-                    height="100%"
+                    /* height="100%" */
+                    sx={{maxWidth: "auto", height: 300}}
+                    /* sx={{display: "block", marginLeft: "auto", marginRight: "auto", width: "100%" }} */
                     image={page.picture}
                     alt={page.alt}
                   />
