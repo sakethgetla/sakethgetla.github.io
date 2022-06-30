7d0b47bcc971c2931a098d4b5a88276114981019
sakethgetla <saketh.getla@protonmail.com>
2022-06-30 12:44:15 +1000

    make resume into jpg from pdf. now much faster!

--- src/About.tsx
+++ src/About.tsx
@@ -1,15 +1,20 @@
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 // import { Document, Page } from 'react-pdf';
+import { Grid } from '@mui/material';
 
 
 
 export default function About() {
   return (
-    <div>
-      {/* <Document file="/static/pdf/SakethResume.pdf"/> */}
-      <Document file="/static/pdf/SakethResume.pdf">
-        <Page pageNumber={1} />
-      </Document>
-    </div>
+    <Grid container padding={10}>
+      <Grid item xs={12}>
+        {/* <Document file="/static/pdf/SakethResume.pdf"/> */}
+        <img src="/static/images/SakethResume.jpg" alt="ire" />
+        {/* </img> */}
+        {/* <Document file="/static/pdf/SakethResume.pdf"> */}
+        {/*   <Page pageNumber={1} /> */}
+        {/* </Document> */}
+      </Grid>
+    </Grid>
   )
 }
