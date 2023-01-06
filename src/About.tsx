2b2fe5e6aa7732c4fbba7ca003054f1797218803
sakethgetla <saketh.getla@protonmail.com>
2023-01-07 10:30:57 +1100

    removed type from resume, format resume.

--- src/About.tsx
+++ src/About.tsx
@@ -1,15 +1,41 @@
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 // import { Document, Page } from 'react-pdf';
-import { Grid } from '@mui/material';
+import { Grid, Card } from '@mui/material';
 
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+function standardImageList() {
+  const item = {
+    img: '/static/images/SakethResume.jpg',
+    title: 'title'
+  };
+  return (
+    // <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+    <ImageList cols={1} >
+      <ImageListItem key={item.img}>
+        <img
+          src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+          srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+          alt={item.title}
+          loading="lazy"
+        />
+      </ImageListItem>
+    </ImageList>
+  );
+}
 
 
 export default function About() {
   return (
     <Grid container padding={10}>
       <Grid item xs={12}>
+
+        {standardImageList()}
         {/* <Document file="/static/pdf/SakethResume.pdf"/> */}
-        <img src="/static/images/SakethResume.jpg" alt="ire" />
+        {/* <Card sx={{ padding: 1, margin: 1, width: 1, height: 1 }} > */}
+        {/*   <img src="/static/images/SakethResume.jpg" alt="ire" /> */}
+        {/* </Card> */}
         {/* </img> */}
         {/* <Document file="/static/pdf/SakethResume.pdf"> */}
         {/*   <Page pageNumber={1} /> */}
