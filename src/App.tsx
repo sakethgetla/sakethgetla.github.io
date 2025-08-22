79df2c54af36540ca8fb448da8d94fa0c6f8de5b
sakethgetla <saketh.getla@protonmail.com>
2025-08-22 12:39:54 +1000

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,58 @@
-import React, { useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 // import { AppBar, Box, Button, Card, CardContent, CardMedia, CardActionArea, Grid, Toolbar, Typography, IconButton } from '@mui/material';
-import { AppBar, Box, Button,  Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 // import { Link as ULink } from '@mui/material';
-import About from "./About"
-import Home from "./Home"
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import About from "./About";
+import Home from "./Home";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 // import { useNavigate } from "react-router-dom";
 // import {Menu} from '@mui/icons-material';
-
 
 function App() {
   // const pages = ['Products', 'Pricing', 'Blog'];
 
   useEffect(() => {
-    document.title = "Saketh Getla"
-    document.
-  }, [])
+    document.title = "Saketh Getla";
+  }, []);
 
   return (
     <>
       <Router>
         <Box sx={{ flexGrow: 1 }}>
-
-          <AppBar enableColorOnDark={true} position="fixed" color="primary" sx={{ broder: 1 }}>
+          <AppBar
+            enableColorOnDark={true}
+            position="fixed"
+            color="primary"
+            sx={{ broder: 1 }}
+          >
             <Toolbar>
-
               <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
                 Saketh Getla
               </Typography>
 
-
-              <a href="https://github.com/sakethgetla" target='_blank' rel="noreferrer" style={{textDecorationLine: 'none', "color": "white" }}>
+              <a
+                href="https://github.com/sakethgetla"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecorationLine: "none", color: "white" }}
+              >
                 {/* <Button color="inherit"> */}
-                <Button color="inherit" startIcon={<OpenInNewIcon/>}>
+                <Button color="inherit" startIcon={<OpenInNewIcon />}>
                   <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     github
                   </Typography>
                 </Button>
               </a>
 
-              <Button component={Link} to={'/'} color="inherit" >
+              <Button component={Link} to={"/"} color="inherit">
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                   Projects
                 </Typography>
               </Button>
               {/* <Button component={Link} to={'/about'} color="inherit" variant="outlined"> */}
-              <Button component={Link} to={'/about'} color="inherit" >
+              <Button component={Link} to={"/about"} color="inherit">
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                   Resume
                 </Typography>
@@ -67,7 +67,6 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </Box>
-
       </Router>
     </>
   );
